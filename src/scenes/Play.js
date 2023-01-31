1849d99664257205a71e17f316b42e1d657aa86d
krasi1 <krissi.petrov@gmail.com>
2023-01-31 18:26:09 +0200

    feat: adding keyboard controls and code gen

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,21 +1,63 @@
-import Footer from "../components/Footer";
 import Scene from "./Scene";
-import { BitmapText } from "pixi.js";
+import Assets from "../core/AssetManager";
+import { keyboard } from "../core/keyboard";
+import gsap from "gsap";
+import { PixiPlugin } from "gsap/all";
+gsap.registerPlugin(PixiPlugin);
 
 export default class Play extends Scene {
   async onCreated() {
-    const footer = new Footer();
-    footer.x = -window.innerWidth / 2;
-    footer.y = window.innerHeight / 2 - footer.height;
-    this.addChild(footer);
+    this.sortableChildren = true;
 
-    const text = new BitmapText("Hello, PIXI", {
-      fontName: "Desyrel",
-    });
+    let bg = PIXI.Sprite.from(Assets._assets.playBg);
+    bg.anchor.set(0.5);
+    bg.scale.set(0.35);
 
-    text.anchor.set(0.5);
+    let closedDoor = PIXI.Sprite.from(Assets._assets.door);
+    closedDoor.anchor.set(0.5);
+    closedDoor.scale.set(0.35);
+    closedDoor.position.set(20, -10);
 
-    this.addChild(text);
+    let handle = PIXI.Sprite.from(Assets._assets.handle);
+    handle.anchor.set(0.5);
+    handle.scale.set(0.35);
+    handle.position.set(-12, -10);
+    handle.zIndex = 1;
+
+    let handleShadow = PIXI.Sprite.from(Assets._assets.handleShadow);
+    handleShadow.anchor.set(0.5);
+    handleShadow.scale.set(0.35);
+    handleShadow.position.set(-7, 0);
+
+    this.addChild(bg, closedDoor, handle, handleShadow);
+
+    function generateCode() {
+      let sides = ["clockwise", "counterclockwise"];
+
+      let combinations = [];
+
+      function combination(rotation, side) {
+        this.rotation = rotation;
+        this.side = side;
+      }
+
+      for(let i = 0; i <= 2; i++){
+        let comb = new combination(Math.floor(Math.random() * (9 - 1) + 1), sides[Math.floor(Math.random() * 2)]);
+        combinations.push(comb);
+        console.log(combinations[i].rotation, combinations[i].side);
+      } 
+
+      return combinations;
+    
+    }
+
+    generateCode();
+
+    const left = keyboard("ArrowLeft"),
+    right = keyboard("ArrowRight");
+
+
+    
   }
 
   /**
