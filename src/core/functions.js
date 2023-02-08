69cc08e68372e1d1bb2748b0c18c6e6ec22af38a
krasi1 <krissi.petrov@gmail.com>
2023-02-08 22:22:00 +0200

    fix: making win async and cleaning up game logic

--- src/core/functions.js
+++ src/core/functions.js
@@ -1,36 +1,25 @@
+/* eslint-disable semi */
 
-/**
- * function that generates an array of objects that have a degree and side property and logs them for the player to see 
- * @param {*} combinations 
- * 
- */
-export function generateCode(combinations) {
-    let sides = ["clockwise", "counterclockwise"];
-
-    function combination(rotation, side) {
-      this.rotation = rotation;
-      this.side = side;
-    }
-
-    for (let i = 0; i <= 2; i++) {
-      combinations.push(new combination(Math.floor(Math.random() * (10 - 1) + 1),sides[Math.floor(Math.random() * 2)]));
-      console.log(combinations[i].rotation, combinations[i].side);
-    }
-
-    return combinations;
+export class Combination {
+  constructor (rotation, side) {
+    this.rotation = rotation;
+    this.side = side;
   }
+}
 
-  /**
-   * simple function that adjusts the necessary parameters of all the sprites on screen
-   * all assets are a bit too large so their scale is set to .35 
-   * @param {*} sprite 
-   * @param {*} x 
-   * @param {*} y 
-   * @param {*} zindex 
-   */
-  export function spriteSetup(sprite, x, y, zindex) {
-    sprite.anchor.set(0.5);
-    sprite.scale.set(0.35);
-    sprite.position.set(x, y);
-    sprite.zIndex = zindex;
+export function generateCode (combinations) {
+  const sides = ['clockwise', 'counterclockwise'];
+
+  for (let i = 0; i <= 2; i++) {
+    combinations.push(new Combination(Math.floor(Math.random() * 9 + 1) * 60, sides[Math.floor(Math.random() * 2)]));
+    console.log(combinations[i].rotation / 60, combinations[i].side);
   }
+  return combinations;
+}
+
+export function spriteSetup (sprite, x, y, zindex) {
+  sprite.anchor.set(0.5);
+  sprite.scale.set(0.35);
+  sprite.position.set(x, y);
+  sprite.zIndex = zindex;
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,172 +1,155 @@
-import Scene from "./Scene";
-import Assets from "../core/AssetManager";
-import { keyboard } from "../core/keyboard";
-import gsap from "gsap";
-import { PixiPlugin } from "gsap/all";
-import {generateCode, spriteSetup } from "../core/functions"
-gsap.registerPlugin(PixiPlugin);
+/* eslint-disable semi */
+/* eslint-disable no-undef */
+import Scene from './Scene'
+import Assets from '../core/AssetManager'
+import { keyboard } from '../core/keyboard'
+import gsap from 'gsap'
+import { PixiPlugin } from 'gsap/all'
+import { generateCode, spriteSetup, Combination } from '../core/functions'
+gsap.registerPlugin(PixiPlugin)
 
 export default class Play extends Scene {
- 
-  async onCreated() {
-    this.accessible = true;
-    this.sortableChildren = true;
-    let bg = PIXI.Sprite.from(Assets._assets.playBg),
-      closedDoor = PIXI.Sprite.from(Assets._assets.door),
-      handle = PIXI.Sprite.from(Assets._assets.handle),
-      handleShadow = PIXI.Sprite.from(Assets._assets.handleShadow),
-      openDoor = PIXI.Sprite.from(Assets._assets.doorOpen),
-      openDoorShadow = PIXI.Sprite.from(Assets._assets.doorOpenShadow),
-       timer = new PIXI.Text( "0", {
-        fontSize: 50,
-        fontStyle: "oblique",
-        fontWeight: 200,
-        align:"center",
-        fill: "#336152",
-      });
-      timer.anchor.set(0.5, 8.5);
-      timer.scale.set(1);
+  async onCreated () {
+    this.accessible = true
+    this.sortableChildren = true
+    const bg = PIXI.Sprite.from(Assets._assets.playBg);
+    const closedDoor = PIXI.Sprite.from(Assets._assets.door);
+    const handle = PIXI.Sprite.from(Assets._assets.handle);
+    const handleShadow = PIXI.Sprite.from(Assets._assets.handleShadow);
+    const openDoor = PIXI.Sprite.from(Assets._assets.doorOpen);
+    const openDoorShadow = PIXI.Sprite.from(Assets._assets.doorOpenShadow);
+    const timer = new PIXI.Text('0', {
+      fontSize: 25,
+      fontWeight: 50,
+      align: 'center',
+      fill: '#dbdfb9'
+    });
+    timer.anchor.set(0.5);
+    timer.position.set(closedDoor.x - 410, closedDoor.y - 51);
 
     spriteSetup(bg, 0, 0, 0);
     spriteSetup(closedDoor, 20, -10, 0);
     spriteSetup(handle, -12, -10, 1);
     spriteSetup(handleShadow, -7, 0, 0);
     spriteSetup(openDoor, 515, -10, 3);
-    spriteSetup(openDoorShadow, 530, 10, 2)
+    spriteSetup(openDoorShadow, 530, 10, 2);
 
     this.addChild(bg, closedDoor, handle, handleShadow);
-    
-    let combinations = [],
-    scene = this,
-    timerCounter = 0; 
-    generateCode(combinations);
-    startTimer();
-    
-    const left = keyboard("ArrowLeft"),
-      right = keyboard("ArrowRight");
-    //necessary vars for gamelogic.  
-    let counter = 0,
-      victoryCount = 0,
-      timeout,
-      timeoutIsActive = false,
-      leftPressed = false,
-      rightPressed = false;
 
-    //handlers for  the left and right arrow keys via the keyboard function imported from keyboard.js 
+    let gameCombinations = [];
+    let playerCombinations = [];
+    const scene = this;
+    let timerCounter = 0;
+    generateCode(gameCombinations);
+    startTimer();
+
+    const left = keyboard('ArrowLeft');
+    const right = keyboard('ArrowRight');
+
+    let counter = 0;
+    let timeout;
+    let timeoutIsActive = false;
+    let leftPressed = false;
+    let rightPressed = false;
+
     left.press = () => {
-      rightPressed?()=>{
-      reset()}
-      :gameLogic("counterclockwise", [-0, -60, -120, -180, -240, -300, -360, -420, -480, -540]);
-      leftPressed = true;   
+      if (!rightPressed) {
+        gameLogic('counterclockwise', -60);
+        leftPressed = true;
+      }
     };
 
     right.press = () => {
-      leftPressed?()=>{
-      reset()}
-      :gameLogic("clockwise", [0, 60, 120, 180, 240, 300, 360, 420, 480, 540]);
-      rightPressed = true;
+      if (!leftPressed) {
+        gameLogic('clockwise', 60);
+        rightPressed = true;
+      }
     };
 
-    /**
-     * main game logic. Variables are updated based on the key pressed and then are compared to the generated key code via a switch statement.
-     * variables are compared within a setTimeout that gets cleared and called again everytime a key is pressed and the functions are called after inputs from the player stop for 1.5 seconds
-     *  @param deg array of possible degrees from 1 to 9 times 60. negative for counterclockwise positive for clockwise
-     *  @param spinSide 
-     */
-    
-    function gameLogic(spinSide, deg){
-
+    async function gameLogic (spinSide, deg) {
       if (counter < 9) {
         counter++;
       }
-      if(timeoutIsActive){
-        clearTimeout(timeout)
+      if (timeoutIsActive) {
+        clearTimeout(timeout);
       }
-       timeoutIsActive = true;
-       timeout = setTimeout(() => {
-          switch (deg[counter] == deg[combinations[victoryCount].rotation] && spinSide == combinations[victoryCount].side) {
-            case true:
-              rightPressed = false;
-              leftPressed = false;
-              counter = 0;
-              gsap.to(handle, { pixi: { rotation: 0 }, duration: 1 });
-              gsap.to(handleShadow, { pixi: { rotation: 0 }, duration: 1 });
-              victoryCount++;
-              break;
-            case false:
-              reset();
-          }
-          if (victoryCount == 3) {
-            win();
-          }
-        }, 1500);
+      timeoutIsActive = true;
+      timeout = setTimeout(() => {
+        playerCombinations.push(
+          new Combination(Math.abs(deg) * counter, spinSide)
+        );
+        rightPressed = false;
+        leftPressed = false;
+        counter = 0;
+        gsap.timeline()
+          .to(handle, { pixi: { rotation: 0 }, duration: 0.5 })
+          .to(handleShadow, { pixi: { rotation: 0 }, duration: 0.5 }, '<');
+        if (playerCombinations.length === 3 && JSON.stringify(gameCombinations) === JSON.stringify(playerCombinations)) {
+          win();
+          console.log('Congratulations!')
+        } else if (playerCombinations.length === 3) {
+          reset();
+          console.log('Wrong! Try again.');
+        }
+      }, 1000);
 
-      
-      gsap.to(handle, { pixi: { rotation: deg[counter] }, duration: 0.3 });
-      gsap.to(handleShadow, { pixi: { rotation: deg[counter] }, duration: 0.3 });
+      gsap.timeline().to(handle, { pixi: { rotation: deg * counter }, duration: 0.3 })
+        .to(handleShadow, { pixi: { rotation: deg * counter }, duration: 0.3 }, '<');
     }
-    /**
-     * resets all the varibles used in gameLogic and generates a new code
-     * also plays a gsap animation where the handle "spins like crazy"
-     */
-    
-    function reset() {
+
+    function reset () {
+      console.clear();
       clearTimeout(timeout);
-      timerCounter = -1; 
-      combinations = [];
-      victoryCount = 0;
       counter = 0;
+      timerCounter = -1;
+      gameCombinations = [];
+      playerCombinations = [];
       rightPressed = false;
       leftPressed = false;
-      gsap.fromTo(handle,{ pixi: { rotation: 720 }}, { pixi: { rotation: 0 }, duration: 1 });
-      gsap.to(handleShadow,{ pixi: { rotation: 720 }}, { pixi: { rotation: 0 }, duration: 1 });
-      generateCode(combinations);  
-    }
-    /**
-     * starts a countdown that updates the text ot @timer every second
-     */
-    
-    function startTimer(){     
-      scene.addChild(timer);
-  
-      setInterval(()=> {
-        timerCounter += 1;
-        timer.text = timerCounter;   
-    }, 1000);
 
+      gsap.timeline()
+        .fromTo(handle, { pixi: { rotation: 720 } }, { pixi: { rotation: 0 }, duration: 1 })
+        .fromTo(handleShadow, { pixi: { rotation: 720 } }, { pixi: { rotation: 0 }, duration: 1 }, '<');
+      generateCode(gameCombinations);
     }
-    /**
-     * fades the background in and out while removing the sprites from the playing scene and adding the ones for the winning scene
-     * after a 6 seconds delay the fading in is reversed and the playing scene is reset
-     */
-    
-    function win() {
-      let blink = PIXI.Sprite.from(Assets._assets.blink);
-      spriteSetup(blink, -50, 0, 3)
-      
-      let tl = gsap.timeline() 
-      tl.fromTo(openDoor, { pixi: { brightness: 0 } }, { pixi: { brightness: 1 }, delay: 1 , duration: 5 });
-      tl.fromTo(openDoorShadow,{ pixi: { brightness: 0 } },{ pixi: { brightness: 1 },delay:1,  duration: 5 }, 0);
-      tl.fromTo(bg,{ pixi: { brightness: 0 } },{ pixi: { brightness: 1 }, delay: 1, duration: 5 }, 0);
-      tl.fromTo(blink, {pixi:{brightness: 1}}, {pixi:{brightness: 1.2,alpha: 1 }, duration: 2, yoyo: true, repeat: -1})
-      
-      setTimeout(() => {
-          tl.reverse().then(()=>{
-            gsap.to(bg, {pixi:{brightness: 1}, duration: 0.1})
-            scene.addChild(closedDoor, handle, handleShadow);
-            scene.removeChild(openDoor, openDoorShadow, blink);
-            setTimeout(()=>{
-              reset();
-              scene.addChild(timer);
-            }, 500)
-    
-          });     
-      }, 6000);
-      
+
+    function startTimer () {
+      scene.addChild(timer);
+      setInterval(() => {
+        timerCounter += 1;
+        timer.text = timerCounter;
+      }, 1000);
+    }
+
+    async function win () {
+      const blink = PIXI.Sprite.from(Assets._assets.blink);
+      spriteSetup(blink, -50, 0, 4);
       scene.addChild(openDoor, openDoorShadow, blink);
       scene.removeChild(closedDoor, handle, handleShadow, timer);
-    }
-     
-  }
 
+      const tl = gsap
+        .timeline()
+        .fromTo(bg, { pixi: { brightness: 0 } }, { pixi: { brightness: 1 }, duration: 2.5 }, 1)
+        .fromTo(openDoor, { pixi: { brightness: 0 } }, { pixi: { brightness: 1 }, duration: 2.5 }, '<')
+        .fromTo(openDoorShadow, { pixi: { brightness: 0 } }, { pixi: { brightness: 1 }, duration: 2.5 }, '<')
+        .fromTo(blink, { pixi: { alpha: 0 } }, { pixi: { brightness: 1.5, alpha: 1 }, duration: 2.5, yoyo: true }, 2);
+
+      await tl;
+
+      await tl.reverse();
+
+      scene.addChild(closedDoor, handle, handleShadow, timer);
+      scene.removeChild(openDoor, openDoorShadow, blink);
+      await gsap
+        .timeline({ duration: 0.1 })
+        .to(bg, { pixi: { brightness: 1 } }, 0)
+        .fromTo(
+          closedDoor, { pixi: { brightness: 0 } }, { pixi: { brightness: 1 } }, 0)
+        .fromTo(handle, { pixi: { alpha: 0, brightness: 0 } }, { pixi: { alpha: 1, brightness: 1 } }, '<')
+        .fromTo(handleShadow, { pixi: { alpha: 0, brightness: 0 } }, { pixi: { alpha: 1, brightness: 1 } }, '<')
+        .fromTo(timer, { pixi: { brightness: 0 } }, { pixi: { brightness: 1, alpha: 1 } }, '<');
+
+      reset();
+    }
+  }
 }
